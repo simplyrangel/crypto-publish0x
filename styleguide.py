5416e516b8b97b3ff5b9c274e584bdc46a92465e
John Rangel <rangel.john.j@gmail.com>
2021-11-12 14:43:53 -0600

    Matplotlib styleguide module for common Publish0x post figures.

---
+++ styleguide.py
@@ -0,0 +1,41 @@
+"""Plotting styleguide."""
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# Set figures to size recommended by Publish0x for 
+# online and mobile viewing:
+def set_rcparams():
+    plt.rcParams.update(
+        {"font.size": 14, 
+         "figure.figsize": (10,6),
+        "lines.linewidth": 3,
+        })
+
+# add user markings and URL's:
+def add_markings():
+    pass
+
+# class to make saving figures easy:
+class imghelper():
+    def __init__(
+        self,
+        save_dir,
+        img_series,
+        save_flag=True,
+        dpi=100,
+        ):
+        self.img_id = 0
+        self.save_dir = save_dir
+        self.img_series = img_series
+        self.save_flag = save_flag
+        self.dpi = dpi
+    
+    def savefig(self):
+        img_name = "%s/%s-%d.png"%(
+            self.save_dir,
+            self.img_series,
+            self.img_id,
+            )
+        if self.save_flag:
+            plt.savefig(img_name,dpi=self.dpi)
+            self.img_id += 1
