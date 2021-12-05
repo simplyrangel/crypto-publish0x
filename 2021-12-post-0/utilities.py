1d167b2b3a4f17b163cc12eaa6d3c525305e21d0
John Rangel <rangel.john.j@gmail.com>
2021-12-05 15:26:55 -0600

    Weekly top 300 coin market cap data scraped from cmc since Jan. 1, 2020.

--- 2021-12-post-0/utilities.py
+++ 2021-12-post-0/utilities.py
@@ -1,88 +1,73 @@
 import numpy as np 
 import pandas as pd
+import json
 from datetime import datetime
 from subprocess import Popen, PIPE
 import time
 import matplotlib.pyplot as plt
 
-def html_filter(stdout):
-    
-    # decode bytestring into UTF-8:
-    s = stdout.decode("UTF-8")
-    
-    # extract relevant section of HTML string output:
-    l = s.find("listingHistorical")
-    sdata = s[l:]
-    
-    # remove coins based on other blockchains:
-    sdata = sdata.split("name")[1:]
-    sdata = [
-        x for x in sdata 
-        if "num_market_pairs" in x
-        ]
-    return sdata
-    
-def extract_data(l):
-
-    # remove needless characters:
-    needless_chars = [
-        "{",
-        "}",
-        "[",
-        "]",
-        '"',
-    ]
-    for c in needless_chars:
-        l = l.replace(c,"")
-    symbol = l.split(",")[1].replace("symbol:","")
-    name = l.split(",")[2].replace("slug:","")
-    result = [symbol, name]
-    return result
-
-def scrape(
-    sunday_range,
+def scrape_cmc_historical(
+    date,
+    limit,
+    convert="USD",
     pause_seconds=5, #s
     ):
+    # define base url:
+    base_url = [
+        "https://web-api.coinmarketcap.com/v1",
+        "cryptocurrency/listings/historical",
+        ]
+    base_url = "/".join(base_url)
     
-    # iterate over each date:
-    frames = []
-    url_template = "https://coinmarketcap.com/historical/%s/"
-    for date in sunday_range:
-        
-        # create URL:
-        url = url_template%(date.strftime("%Y%m%d"))
-        print("scraping %s..."%url)
-        
-        # query URL via CURL:
-        cmd = ["curl",url]
-        p = Popen(cmd,stderr=PIPE,stdout=PIPE)
-        stdout,stderr = p.communicate()
-        
-        # extract coin market cap data:
-        coin_symbols = []
-        coin_names = []
-        output = html_filter(stdout)
-        for l in output:
-            symbol,name = extract_data(l)
-            coin_symbols.append(symbol)
-            coin_names.append(name)
-        
-        # store market cap data in a Pandas Dataframe:
-        num_coins = len(coin_symbols) + 1
-        df = pd.DataFrame(
-            [coin_symbols,coin_names],
-            index=["symbol","name"],
-            columns=range(1,num_coins)
-            ).transpose()
-        df.index.names=["rank"]
-        frames.append(df)
-        
-        # wait several seconds to avoid website query 
-        # limits:
-        time.sleep(pause_seconds) 
+    # create query URL:
+    date_str = date.strftime("%Y-%m-%d")
+    options = "convert=%s&date=%s&limit=%d&start=1"%(
+        convert,
+        date_str,
+        limit,
+        )
+    query = "%s?%s"%(base_url,options)
+    
+    # query via CURL:
+    cmd = ["curl",query]
+    p = Popen(cmd,stderr=PIPE,stdout=PIPE)
+    stdout,stderr = p.communicate()
+    
+    # return stdout:
+    return stdout
 
-    # return data as multiindex dataframe:
-    return pd.concat(frames,keys=sunday_range)
+def process_json(stdout):
+    output = json.loads(stdout)
+    data = output["data"]
+    frames = []
+    
+    # list comprehension for faster extraction:
+    [_process_data_entry(entry,frames) for entry in data]
+    
+    # concatenate results into a Pandas' dataframe 
+    # and return:
+    results = pd.concat(
+        frames,axis=1,
+        ).transpose(
+        ).set_index("name"
+        )
+    return results
+
+def _process_data_entry(entry,frames):
+    quote = entry["quote"]["USD"]
+    s = pd.Series(quote)
+    s["name"] = entry["name"]
+    s["symbol"] = entry["symbol"]
+    s["cmc_rank"] = entry["cmc_rank"]
+    frames.append(s)
+
+
+
+
+
+
+
+
 
 
 
